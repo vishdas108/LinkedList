c5c5a937d0f51fdd91575ee4c8b4d6724ba67dbe
Vishwanath Pratap R <visr0617@netcracker.com>
2019-04-17 14:24:29 +0530

    test2

--- LinkedList/src/App.java
+++ LinkedList/src/App.java
@@ -1,3 +1,21 @@
 public class App {
 
+
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+
+        ll.insertEnd(1);
+        ll.insertEnd(2);
+        ll.insertEnd(3);
+        ll.insertEnd(4);
+        ll.insertEnd(5);
+        ll.insertEnd(6);
+        ll.insertEnd(7);
+        ll.insertEnd(8);
+        ll.insertEnd(9);
+        ll.insertEnd(10);
+
+        ll.insertMiddle(11);
+        ll.traverse();
+    }
 }
--- LinkedList/src/LinkedList.java
+++ LinkedList/src/LinkedList.java
@@ -27,12 +27,51 @@ public class LinkedList {
 
         LinkedListNode traversingNode = head;
 
-            while (traversingNode!=null){
+            while (traversingNode!=null && traversingNode.nextNode!=null){
                 traversingNode= traversingNode.nextNode;
             }
 
             traversingNode.nextNode = newNode;
 
     }
+
+    public void insertMiddle(int data){
+        LinkedListNode newNode= new LinkedListNode(data);
+        LinkedListNode smallPointer=head;
+        LinkedListNode bigPointer=head;
+
+
+
+
+
+        LinkedListNode traverseNode = head;
+
+
+        while (bigPointer!=null && bigPointer.nextNode!=null && bigPointer.nextNode.nextNode!=null){
+            smallPointer= smallPointer.nextNode;
+            bigPointer= bigPointer.nextNode.nextNode;
+
+        }
+        System.out.println("mid point::  " + smallPointer.data);
+        System.out.println("end point::  " + bigPointer.data);
+
+        LinkedListNode tempNode = smallPointer.nextNode;
+        smallPointer.nextNode=newNode;
+        newNode.nextNode=tempNode;
+    }
+
+    public void traverse(){
+
+        LinkedListNode traverseNode = head;
+
+        while (traverseNode!=null){
+
+            System.out.println(traverseNode.data);
+            traverseNode = traverseNode.nextNode;
+        }
+    }
+
+
+
 
 }
