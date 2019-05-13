756046728f08fd19601f03cd64ebe7b25cd7d5ec
Vishwanath Pratap R <visr0617@netcracker.com>
2019-05-13 11:28:36 +0530

    test23

--- LinkedList/src/com/linkedlist/com/strings/StringProblems.java
+++ LinkedList/src/com/linkedlist/com/strings/StringProblems.java
@@ -2,7 +2,26 @@ package com.linkedlist.com.strings;
 
 public class StringProblems {
 
-    public static void main(String[] args) {
+        public static StringProblems INSTANCE = new StringProblems();
 
+    public static void main(String[] args) {
+        INSTANCE.moveAllUpperCaseToEnd("Hello Vishwa");
+
+    }
+
+    public void moveAllUpperCaseToEnd(String input){
+
+        StringBuilder builder = new StringBuilder();
+
+        //useRegularExpression.
+        // in the string replace all the upper characters with empty,
+        // next replace all small letters into empty
+        // EX; input= "Hello Vishwa"
+        //  input.replaceAll("[A-Z]", "")= elloishwa
+        // input.replaceAll("[^A-Z]", "") = HV
+        // concat both ello ishwaHV
+        String result = input.replaceAll("[A-Z]", "") + input.replaceAll("[^A-Z]", "");
+
+        System.out.println("result: " + result);
     }
 }
